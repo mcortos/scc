7a826f6d7b1ffd5560b260b3dc5c22fa2c1eca6e
Mcortos <maximecortesi@gmail.com>
2018-05-07 07:56:32 +0100

    test

--- index.php
+++ index.php
@@ -121,8 +121,8 @@ $devsupport=($bill*3/100);
           <div class="card-body">
             <h1 class="card-title pricing-card-title"><?php echo ' $ '. $entsuppbill.'';?> <small class="text-muted">/ month</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li><span class="float-left">0-150K</span><span class="float-right"><?php echo '$2000'. $t1.' ';?></span></li>
-              <li><span class="float-left">150-500K</span><span class="float-right"><?php echo '$234555'. $t2.' ';?></span></li>
+              <li><p class="float-left">0-150K</p><p class="float-right"><?php echo '$2000'. $t1.' ';?></p></li>
+              <li><p class="float-left">150-500K</p><p class="float-right"><?php echo '$234555'. $t2.' ';?></p></li>
               <br>
               <li>Tiers 3 : <?php echo '500K-01M $'. $t3.' ';?></li>
               <li>Tiers 4 : <?php echo '+01M    &emsp;&emsp;&emsp;&emsp;&emsp;    $'. $t4.' ';?></li>
