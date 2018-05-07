f6f2403bba28da7de2180bc8e3b7b9ea19254cb8
Mcortos <maximecortesi@gmail.com>
2018-05-07 09:58:13 +0200

    test

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 if(isset($_POST['submit'])){
     $bill=$_POST['bill'];
-   
-    if(isset($_POST['discount']) AND !isset($_POST['bill']) ){
-    $display="none";
-    }else{
     $display="inline";
-    }
-
 
     if(isset($_POST['discount'])){
       $discount=$_POST['discount'];
